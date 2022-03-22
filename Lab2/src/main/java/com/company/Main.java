8b14cb6b88656f487b0426c8f2a4d1042a8ef68a
unknown <ciobanu.corina@isa.utm.md>
2022-03-22 21:26:50 +0200

    deleted unnecessary code

--- Lab2/src/main/java/com/company/Main.java
+++ Lab2/src/main/java/com/company/Main.java
@@ -7,13 +7,5 @@ public class Main {
 
     public static void main(String[] args) throws MessagingException, IOException {
         new AppUI();
-//        EmailSender emailSender = new EmailSender("ciobanu.corina@isa.utm.md",
-//                "testingemailacount@gmail.com",
-//                "smtp.gmail.com",
-//                "587",
-//                "testingAccount01");
-//        emailSender.sendEmail();
-//        EmailReceiver emailReceiver = new EmailReceiver("imap.gmail.com", "993","testingemailacount@gmail.com", "testingAccount01");
-//        emailReceiver.getInbox();
     }
 }
